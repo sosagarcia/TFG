6c227a7bdfbf231d72973cf1dcf481c7e42716bd
John DoeRenato <renmanzo@hotmail.com>
2019-08-18 00:44:03 +0100

    AVoid email

--- status.py
+++ status.py
@@ -46,8 +46,8 @@ def alarmaCheck():
             text = "Se ha registrado una alarma, la distancia es de " + \
                 str(distancia) + " cm."
             write_log(text, aPath, aName)
-            feedback = sendEmail(
-                str(text), "monitycont@gmail.com", "Alarma Registrada")
+            # feedback = sendEmail(
+            #  str(text), "monitycont@gmail.com", "Alarma Registrada")
             textD = str(distancia) + " cm."
             write_log(textD, disPath, dName)
             GPIO.output(ledA, False)
