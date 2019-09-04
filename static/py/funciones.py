5083bc1659c3db7bfe62019b8b0714b1ff2beb44
John DoeRenato <renmanzo@hotmail.com>
2019-09-04 23:34:53 +0100

    asdasd

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -218,6 +218,8 @@ def openAllBig(path, inicio, fin):
                 lenName = len(name)
                 nombre = name [lenPath : lenName]
                 fechaTemp = datetime(year = int(nombre[6:10]), month = int(nombre[3:5]), day = int(nombre[0:2]))
+                return inicio
+                
                 if (inicio <= fechaTemp):
                     logLines.append(f.readlines())
         except IOError as exc:
