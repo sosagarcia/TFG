534afbbc3fb9c71f02c35a8d2e5be212dd1e41d3
John DoeRenato <renmanzo@hotmail.com>
2019-09-04 21:02:03 +0100

    asd

--- index.py
+++ index.py
@@ -383,7 +383,7 @@ def chart():
         linea = logLines[i]
         fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
 
-        if( inicio < fechaTemp):
+        if( inicio <= fechaTemp):
             subresult.append(linea)
     print(subresult)
  
