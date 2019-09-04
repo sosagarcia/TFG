152935c0b5ea86b9b5c1e850292e1c7f5fd61d0c
John DoeRenato <renmanzo@hotmail.com>
2019-09-04 23:31:43 +0100

    asd

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -219,7 +219,6 @@ def openAllBig(path, inicio, fin):
                 nombre = name [lenPath : lenName]
                 fechaTemp = datetime(year = int(nombre[6:10]), month = int(nombre[3:5]), day = int(nombre[0:2]))
                 if (inicio < fechaTemp):
-                    return "Coño"
                     logLines.append(f.readlines())
         except IOError as exc:
             if exc.errno != errno.EISDIR:
