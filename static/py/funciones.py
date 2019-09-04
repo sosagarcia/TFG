feee8d2015b8c2c4e0d13d2aa71a1c6df267d2f2
John DoeRenato <renmanzo@hotmail.com>
2019-09-04 22:25:44 +0100

    sdf

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -188,7 +188,7 @@ def getLogsD(path, name, fecha, muestras):
     inicio, fin = divideFechas (fecha)
     logLines = openAll(path)
     max =len(logLines)
-    for i in range(0, max):
+    for i in range(0, max, 1000):
         linea = logLines[i]
         fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
         if inicio < fechaTemp:
