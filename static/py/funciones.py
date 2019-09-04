59079843ff84994fd09ea1f5910627748a2d350f
John DoeRenato <renmanzo@hotmail.com>
2019-09-04 21:16:48 +0100

    asdasdasd

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -158,7 +158,6 @@ def getLogs(path, name, fecha, muestras):
 
     if (str(fecha) == "*"):
        logLines = openAll(path)
-       return (logLines, "0")
     else :
         log = open(str(path) + str(fecha) + str(name), "r")
         logLines = log.readlines()
@@ -188,12 +187,12 @@ def getLogsD(path, name, fecha, muestras):
     subresult = list()
     inicio, fin = divideFechas (fecha)
     logLines = openAll(path)
-    return(logLines, "0")
     for i in range(0, len(logLines)):
         linea = logLines[i]
         fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
         if( inicio <= fechaTemp):
             subresult.append(linea)
+    return (subresult,muestras)
     fechas, valores = determina(subresult, muestras)
     return (fechas, valores)
 
