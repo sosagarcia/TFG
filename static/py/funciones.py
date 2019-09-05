b89150b1f6d8f311e3f2fee325b7c086dd37900a
John DoeRenato <renmanzo@hotmail.com>
2019-09-05 09:58:04 +0100

    asd

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -192,15 +192,18 @@ def getLogsD(path, name, fecha, muestras):
     dias = len(logLines)
     if dias == 0:
         return ("0","0")
-    if dias == 1 :
-        dia = logLines[0]
-        max = len(dia)
-        for i in range(0, max):
-            linea = dia[i]
-            fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
-            if (inicio < fechaTemp < fin):
-                subresult.append(linea)
+    if dias < 3 :
+        for i in range(0, dias):
+            dia = logLines[i]
+            max = len(dia)
+            for i in range(0, max):
+                linea = dia[i]
+                fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
+                
+                if (inicio < fechaTemp < fin):
+                    subresult.append(linea)
     return (subresult, subresult)
+    
     #if len(logLines) == 2 :
         
             
