fd8ad09ca9e8eaa33d03aadb82a31d6cd13e8239
John DoeRenato <renmanzo@hotmail.com>
2019-09-07 17:16:13 +0100

    asd

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -209,38 +209,30 @@ def getLogsD(path, name, fecha, muestras):
             max = len(dia)
             for i in range(0, max):
                 linea = dia[i]
-                if not (linea[20:24] == "Erro") and (linea[10:11] == " "):         
-                    fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
-                    if (inicio < fechaTemp < fin):
-                        subresult.append(linea)
-    if dias >= 3 : 
+                fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
+                if (inicio < fechaTemp < fin):
+                    subresult.append(linea)
+    if dias <= 3 : 
         dia = logLines[0]
         max = len(dia)
         for i in range(0, max):
             linea = dia[i]
-            if not (linea[20:24] == "Erro") and (linea[10:11] == " "): 
-                fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour= int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
-                if (inicio < fechaTemp < fin):
-                    subresult.append(linea)
-                
-        
-        for i in range(1, (dias -1)):
+            fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour= int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
+            if (inicio < fechaTemp < fin):
+                subresult.append(linea)
+        for i in range(1, dias -1):
             dia = logLines[i]
             max = len(dia)
             for i in range(0, max):
                 linea = dia[i]
                 subresult.append(linea)
-                
-        
         dia = logLines[dias -1]
         max = len(dia)
         for i in range(0, max):
             linea = dia[i]
-            if not (linea[20:24] == "Erro") and (linea[10:11] == " "):
-                fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour= int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
-                if (inicio < fechaTemp < fin):
-                    subresult.append(linea)
-              
+            fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour= int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
+            if (inicio < fechaTemp < fin):
+                subresult.append(linea)
     fechas, valores = determina(subresult, muestras)
     return (fechas, valores)
     
