4f77091fb9f3748923eeec69e5d3b4c916f9179b
John DoeRenato <renmanzo@hotmail.com>
2019-09-07 18:01:10 +0100

    <=

--- static/py/funciones.py
+++ static/py/funciones.py
@@ -211,7 +211,7 @@ def getLogsD(path, name, fecha, muestras):
                 linea = dia[j] 
                 if not (linea[20:24] == "Error") and (linea[10:11] == " "):
                     fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour = int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
-                    if (inicio < fechaTemp < fin):
+                    if (inicio < fechaTemp <= fin):
                         subresult.append(linea)
     if dias >= 3 : 
         for k in range(0, dias):
@@ -222,7 +222,7 @@ def getLogsD(path, name, fecha, muestras):
                     linea = dia[i]
                     if not (linea[20:24] == "Error") and (linea[10:11] == " "):
                         fechaTemp = datetime(year = int(linea[6:10]), month = int(linea[3:5]), day = int(linea[0:2]), hour= int(linea[11:13]), minute = int(linea[14:16]), second = int(linea[17:19]))
-                        if (inicio < fechaTemp < fin):
+                        if (inicio < fechaTemp <= fin):
                             subresult.append(linea)
             else :            
                 max = len(dia)
