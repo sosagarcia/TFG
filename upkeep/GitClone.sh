250c62c32fa501f90e43e630945519c988ec505a
John DoeRenato <renmanzo@hotmail.com>
2019-09-07 20:36:38 +0100

    as

---
+++ upkeep/GitClone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo service apache2 stop &
+cd
+cd /var/www/html/
+echo "Borrando"
+sudo rm -R TFG/
+echo "Empieza el Clonado"
+sudo git clone https://sosagarcia:nuevacontraseña12@github.com/sosagarcia/TFG.g$
+echo "Permisos"
+cd
+sudo chmod -R 777 /var/www
+echo "Reiniciando Apache"
+sudo service apache2 start &
+echo "Copiando scripts"
+sudo cp -R /var/www/html/TFG/upkeep/* /home/pi/Scripts/
+echo "Permisos Scripts"
+sudo chmod -R 777 /home/pi/Scripts/
