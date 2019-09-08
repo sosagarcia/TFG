1faf9cd17a61de75f0f344d9f15e311af308fab4
John DoeRenato <renmanzo@hotmail.com>
2019-09-08 11:42:47 +0100

    asd

---
+++ upkeep/Actualizar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Empieza la actualización"
+sudo apt update -y
+echo "Empieza la instalación"
+sudo apt-get upgrade -y
+sudo apt dist-upgrade -y
+echo "Reiniciando"
+sudo apt clean -y
+sudo reboot
+
