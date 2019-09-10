87a28b720c8f294b3d29f499eac85c0c9a1e542b
John DoeRenato <renmanzo@hotmail.com>
2019-09-10 15:39:51 +0100

    push

--- upkeep/SaveLogs.sh
+++ upkeep/SaveLogs.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
@@ -6,8 +7,9 @@ echo "Hoy es  $DIA y la hora actual es $HORA"
 
 echo "Empieza el commit"
 cd /var/log/iot/
+sudo git add .
 sudo git commit -m "Hoy es  $DIA y la hora actual es $HORA"
-#sudo git remote add origin https://MonYCon:EmeyCe.123@github.com/MonYCon/logs.git
+#sudo git remote add origin https://MonYCon:EmeyCe.123@github.com/MonYCon/logs.$
 sudo git push -u origin master
 
 
