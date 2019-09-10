4ec2a53a1b8e35599159fb9dcf746b00cd1a425e
John DoeRenato <renmanzo@hotmail.com>
2019-09-10 17:01:31 +0100

    asd

--- upkeep/iniciar.sh
+++ upkeep/iniciar.sh
@@ -3,8 +3,12 @@ DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
 echo "###########################################################################"
 echo "Hoy es  $DIA y la hora actual es $HORA"
-
+echo "iniciando Status.py en segundo plano"
 sudo python3 /var/www/html/TFG/status.py &
+statusPID=$!
+echo "Pid actual de status.py:  $statusPID"
+echo "iniciando worker.py en segundo plano"
 sudo python3 /var/www/html/TFG/worker.py &
- #as
+workerPID=$!
+echo "Pid actual de status.py:  $workerPID"
  echo "###########################################################################"
