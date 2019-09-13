6b8f21ad2f25ba2478f96d73398f4138f040cbd6
John DoeRenato <renmanzo@hotmail.com>
2019-09-13 20:19:02 +0100

    contraste

--- camera.py
+++ camera.py
@@ -6,14 +6,16 @@ def takePicture():
     camera = PiCamera()
     camera.rotation = 180
     # max de fotos: 2592 x 1944, de video : 1920×1080
+    # camera.contrast = i #para el contraste
+    # camera.brightness = n para el brillo , 50 por defecto, un poco mas en entornos oscuros, un poco menos en entornos luminoso
     camera.resolution = (1920, 1080)
     camera.framerate = 15
     camera.annotate_text = "Hello world!"
     camera.start_recording('/home/pi/Desktop/video.h264')
 
     for i in range(100):
-        camera.annotate_text = "Brightness: %s" % i
-        camera.brightness = i
+        camera.annotate_text = "Contrast: %s" % i
+        camera.contrast = i
         sleep(0.1)
     camera.stop_recording()
 
