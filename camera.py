d369cdc940780818385777b28279c02ee054f82f
John DoeRenato <renmanzo@hotmail.com>
2019-09-13 20:03:07 +0100

    bright

--- camera.py
+++ camera.py
@@ -5,9 +5,15 @@ from picamera import PiCamera
 def takePicture():
     camera = PiCamera()
     camera.rotation = 180
-    camera.start_recording('/home/pi/Desktop/video.h264')
-    sleep(5)
-    camera.stop_recording()
+    camera.resolution = (2592, 1944)
+    camera.framerate = 15
+    camera.annotate_text = "Hello world!"
+    camera.start_preview()
+    for i in range(100):
+        camera.annotate_text = "Brightness: %s" % i
+        camera.brightness = i
+        sleep(0.1)
+    camera.stop_preview()
 
 
 if __name__ == '__main__':
