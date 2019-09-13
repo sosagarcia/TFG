4d2c4da20066cd849ccea8973cd6039b991e13e0
John DoeRenato <renmanzo@hotmail.com>
2019-09-13 20:06:38 +0100

    bright

--- camera.py
+++ camera.py
@@ -8,12 +8,13 @@ def takePicture():
     camera.resolution = (2592, 1944)
     camera.framerate = 15
     camera.annotate_text = "Hello world!"
-    camera.start_preview()
+    camera.start_recording('/home/pi/Desktop/video.h264')
+
     for i in range(100):
         camera.annotate_text = "Brightness: %s" % i
         camera.brightness = i
         sleep(0.1)
-    camera.stop_preview()
+    camera.stop_recording()
 
 
 if __name__ == '__main__':
