a431198e1e13cef8c4a42cde529a4e2e634349e2
John DoeRenato <renmanzo@hotmail.com>
2019-09-15 13:18:13 +0100

    asd

--- upkeep/Git.sh
+++ upkeep/Git.sh
@@ -20,6 +20,6 @@ sudo cp -R /var/www/html/TFG/upkeep/* /home/pi/Scripts/
 echo "Permisos Scripts"
 sudo chmod -R 777 /home/pi/Scripts/
 echo "copiando fotos"
-sudo cp -R /var/log/iot/camera/* /var/www/html/TFG/static/img/came
+sudo cp -R /var/log/iot/camera/* /var/www/html/TFG/static/img/camara
 ra
 echo "###########################################################################"
