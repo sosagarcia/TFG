1b1c344aa06808aaf9af5709735df2e6aab9d8f5
John DoeRenato <renmanzo@hotmail.com>
2019-09-17 11:22:20 +0100

    asd

--- worker.py
+++ worker.py
@@ -18,11 +18,29 @@ GPIO.setup(pinA, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(pinB, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(pinC, GPIO.OUT, initial=GPIO.LOW)
 
+
 def inicializa():
-    
+
     GPIO.output(pinA, False)
     GPIO.output(pinB, False)
     GPIO.output(pinC, False)
+
+    while True:
+
+        try:
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user="renato",
+                passwd="renato12",
+                database="flaskcontacts"
+            )
+            break
+        except:
+            hoy = dt.datetime.now()
+            print("Aun no ha sido posible conectar con la base de datos" +
+                  hoy.strftime("%d-%m-%Y_%H:%M:%S"))
+            time.sleep(1)
+            continue
 
 
 switcher = {
@@ -30,22 +48,6 @@ switcher = {
     user2: pinB,
     user3: pinC
 }
-while True:
-
-    try:
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="renato",
-            passwd="renato12",
-            database="flaskcontacts"
-        )
-        break
-    except:
-        hoy = dt.datetime.now()
-        print("Aun no ha sido posible conectar con la base de datos" +
-              hoy.strftime("%d-%m-%Y_%H:%M:%S"))
-        time.sleep(1)
-        continue
 
 
 def updateTaps():
