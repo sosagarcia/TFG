2158de0e9a4a06ff8cb08572eba3ed790ecdb121
John DoeRenato <renmanzo@hotmail.com>
2019-09-18 11:44:19 +0100

    servo

---
+++ Servo.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO  # Importamos la libreria RPi.GPIO
+import time  # Importamos time para poder usar time.sleep
+
+GPIO.setmode(GPIO.BOARD)  # Ponemos la Raspberry en modo BOARD
+GPIO.setup(21, GPIO.OUT)  # Ponemos el pin 21 como salida
+# Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
+p = GPIO.PWM(21, 50)
+p.start(7.5)  # Enviamos un pulso del 7.5% para centrar el servo
+
+try:
+    while True:  # iniciamos un loop infinito
+
+        # Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+        p.ChangeDutyCycle(4.5)
+        time.sleep(0.5)  # pausa de medio segundo
+        # Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+        p.ChangeDutyCycle(10.5)
+        time.sleep(0.5)  # pausa de medio segundo
+        # Enviamos un pulso del 7.5% para centrar el servo de nuevo
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)  # pausa de medio segundo
+
+except KeyboardInterrupt:  # Si el usuario pulsa CONTROL+C entonces...
+    p.stop()  # Detenemos el servo
+    GPIO.cleanup()  # Limpiamos los pines GPIO de la Raspberry y cerramos el script
