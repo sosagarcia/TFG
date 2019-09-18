bd9720b335e6fe73654530e824b24082a1c20623
John DoeRenato <renmanzo@hotmail.com>
2019-09-18 16:09:59 +0100

    asd

--- Servo.py
+++ Servo.py
@@ -5,19 +5,33 @@ GPIO.setmode(GPIO.BCM)  # Ponemos la Raspberry en modo BOARD
 GPIO.setup(13, GPIO.OUT)  # Ponemos el pin 21 como salida
 # Ponemos el 13 en modo PWM y enviamos 50 pulsos por segundo
 p = GPIO.PWM(13, 50)  # Enviamos un pulso del 7.5% para centrar el servo
-
+izq = 2
+cent = 7.5
+der = 14
 try:
       # iniciamos un loop infinito
-    p.start(2)
-    # Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
-    time.sleep(2)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(2)  # pausa de medio segundo
+    p.start(izq)
+
+    time.sleep(1)
+    p.stop()
+    time.sleep(1)
+
+    p.start(cent)
+    time.sleep(1)
+    p.stop()
+    time.sleep(1)
+
+    p.start(der)
+    time.sleep(1)
+    p.stop()
+    time.sleep(1)
+
+    # pausa de medio segundo
     # Enviamos un pulso del 10.5% para girar el servo hacia la derecha
-    p.ChangeDutyCycle(14)
-    time.sleep(2)  # pausa de medio segundo
+    p.start(izq)
+    time.sleep(.05)  # pausa de medio segundo
     # Enviamos un pulso del 7.5% para centrar el servo de nuevo
-    p.ChangeDutyCycle(2)  # pausa de medio segundo
+    # pausa de medio segundo
     p.stop()
 except KeyboardInterrupt:  # Si el usuario pulsa CONTROL+C entonces...
     p.stop()  # Detenemos el servo
