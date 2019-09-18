df4772fbca39c9333f649106ff96435f1eb138f9
John DoeRenato <renmanzo@hotmail.com>
2019-09-18 16:03:26 +0100

    doto

--- Servo.py
+++ Servo.py
@@ -8,19 +8,18 @@ p = GPIO.PWM(13, 50)
 p.start(7.5)  # Enviamos un pulso del 7.5% para centrar el servo
 
 try:
-    while True:  # iniciamos un loop infinito
-
-        # Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
-        p.ChangeDutyCycle(2)
-        time.sleep(2)  # pausa de medio segundo
-        # Enviamos un pulso del 10.5% para girar el servo hacia la derecha
-        p.ChangeDutyCycle(7.5)
-        time.sleep(2)  # pausa de medio segundo
-        # Enviamos un pulso del 7.5% para centrar el servo de nuevo
-        p.ChangeDutyCycle(14)
-        time.sleep(2)
-        p.ChangeDutyCycle(2)  # pausa de medio segundo
-
+      # iniciamos un loop infinito
+    p.start(2)
+    # Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+    time.sleep(2)
+     p.ChangeDutyCycle(7.5)
+    time.sleep(2)  # pausa de medio segundo
+    # Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+    p.ChangeDutyCycle(14)
+    time.sleep(2)  # pausa de medio segundo
+    # Enviamos un pulso del 7.5% para centrar el servo de nuevo
+    p.ChangeDutyCycle(2)  # pausa de medio segundo
+    p.stop()
 except KeyboardInterrupt:  # Si el usuario pulsa CONTROL+C entonces...
     p.stop()  # Detenemos el servo
     GPIO.cleanup()  # Limpiamos los pines GPIO de la Raspberry y cerramos el script
